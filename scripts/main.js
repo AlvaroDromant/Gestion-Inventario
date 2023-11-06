071825be02f1c5203bf3f5a78c1462559656b574
Alvaro <alvarodromantligero@alumnos.tuniversformacion.es>
2023-11-06 11:50:38 +0100

    mejorando el programa

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,5 @@
+import { inventario } from "./products";
+
 document.addEventListener("DOMContentLoaded", listproducts);
 
 document.addEventListener("click", () => {
@@ -8,23 +10,13 @@ document.addEventListener("click", () => {
     });
 });
 
-const buscarProducto = () => {
-    const input = document.getElementById('buscar-producto');
-    const filter = input.value.toUpperCase();
-    const table = document.getElementById('inventario-tabla');
-    const rows = table.getElementsByTagName('tr');
 
-    for (let i = 0; i < rows.length; i++) {
-        const cells = rows[i].getElementsByTagName('td')[0]; // Solo la primera celda (nombre)
-        const cellText = cells.innerText.toUpperCase();
 
-        if (cellText.indexOf(filter) > -1) {
-            rows[i].style.display = '';
-        } else {
-            rows[i].style.display = 'none';
-        }
-    }
-};
+
+
+
+
+
 
 
 
